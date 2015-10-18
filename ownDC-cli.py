55c7297e4f884f19f267f4cb3ecf6f74171caba8
Javier Quinteros <javier@gfz-potsdam.de>
2015-10-18 21:00:00 +0200

    First approach for retry. Read config file and new parameter to wait for retry.

--- ownDC-cli.py
+++ ownDC-cli.py
@@ -9,9 +9,10 @@ from query import DataSelectQuery
 
 
 class SummarizedRun(dict):
-    def __init__(self, tws):
-        for line in tws.splitlines():
-            self[line] = list()
+    def __init__(self, tws=None):
+        if tws is not None:
+            for line in tws.splitlines():
+                self[line] = list()
 
     def __url2nslc(self, url):
         params = parse_qs(urlparse(url).query)
@@ -82,9 +83,12 @@ def main():
     parser.add_argument('-p', '--post-file', default=None,
                         help='File with the streams and timewindows requested.')
     parser.add_argument('-o', '--output', default='request',
-                        help='Filename used to save the data and the logs.')
-    parser.add_argument('-r', '--retries',
+                        help='Filename (without extension) used to save the data and the logs.')
+    parser.add_argument('-r', '--retries', type=int,
                         help='Number of times that data should be requested if there is no answer or if there is an error',
+                        default=0)
+    parser.add_argument('-m', '--minutes',
+                        help='Number of minutes between retries for the lines without data',
                         default=3)
     parser.add_argument('-v', '--verbosity', action="count", default=0,
                         help='Increase the verbosity level')
@@ -97,22 +101,43 @@ def main():
         fh = sys.stdin
 
     lines = fh.read()
-    summary = SummarizedRun(lines)
+    #summary = SummarizedRun(lines)
+    summary = SummarizedRun()
 
     ds = DataSelectQuery(summary,
                          routesFile='data/ownDC-routes.xml',
-                         configFile='ownDC.cfg')
-    iterObj = ds.makeQueryPOST(lines)
+                         configFile=args.config)
 
     outwav = open('%s.mseed' % args.output, 'wb')
-    #outlog = open('%s.log' % args.output, 'w')
 
-    for chunk in iterObj:
-        outwav.write(chunk)
+    # Attempt number to download the waveforms
+    attempt = 0
 
+    while ((attempt <= args.retries) and (len(lines.splitlines()) > 0)):
+        print 'Attempt Nr. %d of %d' % (attempt+1, args.retries+1)
+
+        iterObj = ds.makeQueryPOST(lines)
+        
+        for chunk in iterObj:
+            outwav.write(chunk)
+
+        lines = ''
+        for k, v in summary.iteritems():
+            if sum([l[2] for l in v]) <= 0:
+                lines = '%s%s\n' % (lines, k)
+
+        attempt += 1
+        print 'Summary', summary
+        print 'Lines\n', lines
+
+    
     # FIXME I should decide here a nice format for the output
     # and also if it should be to stdout, a file or a port
-    print summary
+    with open('%s.log' % args.output, 'w') as outlog:
+        for k, v in summary.iteritems():
+            outlog.write('%s %s %d bytes\n' % (k, [le[1] for le in v], sum([le[2] for le in v])))
+
+    outwav.close()
 
 if __name__ == '__main__':
     main()
