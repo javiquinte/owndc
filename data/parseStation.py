37b0c7b276d606fb9e0416ea08f03f4dd7995f59
Javier Quinteros <javier@gfz-potsdam.de>
2016-02-18 21:27:59 +0100

    Include comments in the parseStation method, which seems to work very well.

--- data/parseStation.py
+++ data/parseStation.py
@@ -38,50 +38,41 @@ per object (e.g. one for network, one for each station and so on).
         else:
             event, root = next(context)
 
-        # Check that it is really a StationXML
-        # FIXME This verification should be corrected, checked and enabled again
-        # if root.tag[-len('routing'):] != 'routing':
-        #     msg = '%s seems not to be a routing file (XML). Skipping it!\n' \
-        #         % fileName
-        #     logs.error(msg)
-        #     return ptRT
-
         # Extract the namespace from the root node
         # FIXME This should be checked against IRIS
         namesp = root.tag[:root.tag.find('}') + 1]
 
         for event, netw in context:
-            # The tag of this node should be "Network".
-            # Now it is not being checked because
-            # we need all the data, but if we need to filter, this
-            # is the place.
-            #
+            # Loop through the events and elements in the upper hierarchy
             if event == "end":
-                # if netw.tag != 'Network':
-                #     print 'Error! netw.tag = %s' % netw.tag
-                #     return
-
-                # print netw.tag, namesp
+                # Check that this is a Network
                 if netw.tag == '%sNetwork' % namesp:
 
+                    # Include another element at the top, so that the namespace
+                    # definition is here and NOT in the Network
                     fullXML = ET.ElementTree(ET.Element('FDSNXML'))
                     auxRoot = fullXML.getroot()
+                    # Create an Element based on the original one but without
+                    # namespace in the tag
                     auxRoot.append(ET.Element('Network', netw.attrib))
                     n2Save = auxRoot.find('Network')
 
-                    # Traverse through the sources
+                    # Iterate through the stations
                     for stat in netw:
+                        # Everything at this level that is NOT a Station should
+                        # be added to the Network
                         if stat.tag != '%sStation' % namesp:
                             subNet = ET.Element(stat.tag[len(namesp):],
                                                 stat.attrib)
                             subNet.text = stat.text
-                            # n2Save.append(subNet)
                             n2Save.append(subNet)
 
                         else:
+                            # Create the Station without NS
                             s2Save = ET.Element('Station', stat.attrib)
 
                             for cha in stat:
+                                # Add everything that is not a Channel
                                 if cha.tag != '%sChannel' % namesp:
                                     subSta = ET.Element(
                                         cha.tag[len(namesp):],
@@ -89,9 +80,11 @@ per object (e.g. one for network, one for each station and so on).
                                     subSta.text = cha.text
                                     s2Save.append(subSta)
                                 else:
+                                    # Create the Channel
                                     c2Save = ET.Element('Channel', cha.attrib)
 
                                     for resp in cha:
+                                        # Add everything that is not a Response
                                         if resp.tag != '%sResponse' % namesp:
                                             subCha = ET.Element(
                                                 resp.tag[len(namesp):],
@@ -99,15 +92,24 @@ per object (e.g. one for network, one for each station and so on).
                                             subCha.text = resp.text
                                             c2Save.append(subCha)
                                         else:
+                                            # Create a Response from the
+                                            # original Element. Mainly because
+                                            # we need all the content and we
+                                            # won't iterate to parse it
                                             clearResp = ET.tostringlist(resp)
+                                            # Check in the first 10 lines if
+                                            # there is a NS and delete it
                                             for i, ln in enumerate(clearResp[:10]):
                                                 lns = ln.split()
                                                 lns2 = [x for x in lns if not x.startswith('xmlns:ns0=')]
                                                 clearResp[i] = ''.join(lns2)
 
+                                            # Remove all traces from a NS
                                             for i, ln in enumerate(clearResp):
                                                 clearResp[i] = ln.replace('ns0:', '')
 
+                                            # Save the Response file (includes
+                                            # closing element
                                             with open('%s.%s.%s.resp.xml' %
                                                       (netw.get('code'),
                                                        stat.get('code'),
@@ -117,6 +119,8 @@ per object (e.g. one for network, one for each station and so on).
                                                     ''.join(clearResp))
                                             resp.clear()
 
+                                    # Save the Channel file without the closing
+                                    # element!
                                     with open('%s.%s.%s.xml' %
                                               (netw.get('code'),
                                                stat.get('code'),
@@ -126,12 +130,14 @@ per object (e.g. one for network, one for each station and so on).
                                             c2Save)[:-1]))
                                     cha.clear()
 
+                            # Save the Station file without the closing element!
                             with open('%s.%s.xml' %
                                       (netw.get('code'), stat.get('code')),
                                       'w') as sf:
                                 sf.write(''.join(ET.tostringlist(s2Save)[:-1]))
                             stat.clear()
 
+                    # Save the Network file without the closing element!
                     with open('%s.xml' % netw.get('code'), 'w') as nf:
                         nf.write(''.join(ET.tostringlist(n2Save)[:-1]))
                     netw.clear()
