612de8dda358cb366480c9d0683364f43464ab8f
Javier Quinteros <javier@gfz-potsdam.de>
2016-02-22 17:14:18 +0100

    Comments and cleanup of getEIDAconfig.

--- data/getEIDAconfig.py
+++ data/getEIDAconfig.py
@@ -5,12 +5,7 @@ import sys
 import telnetlib
 import argparse
 from time import sleep
-import xml.etree.cElementTree as ET
-
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
+import logging
 
 try:
     import configparser
@@ -18,12 +13,8 @@ except ImportError:
     import ConfigParser as configparser
 
 sys.path.append('..')
-
-from utils import addRemote
-from utils import addRoutes
 from utils import Route
 from utils import RoutingCache
-import logging
 
 """
 .. todo::
@@ -32,6 +23,7 @@ import logging
     and services following the same table that we had previously.
 
 """
+
 
 def mapArcFDSN(route):
     """Map from an Arclink address to a Dataselect one
@@ -80,9 +72,10 @@ def mapArcFDSN(route):
         return koeri
     raise Exception('No FDSN-WS equivalent found for %s' % route)
 
+
 def arc2fdsnws(filein, fileout, config='../ownDC.cfg'):
     """Read the routing file in XML format and add the Dataselect and Station
-routes based on the Arclink information. The resulting table is stored in 
+routes based on the Arclink information. The resulting table is stored in
 
 :param filein: Input file with routes (usually from an Arclink server).
 :type filein: str
@@ -108,7 +101,8 @@ routes based on the Arclink information. The resulting table is stored in
     rc.toXML(fileout)
 
 
-def getArcRoutes(arcServ='eida.gfz-potsdam.de', arcPort=18001, foutput='routing.xml'):
+def getArcRoutes(arcServ='eida.gfz-potsdam.de', arcPort=18001,
+                 foutput='routing.xml'):
     """Connects via telnet to an Arclink server to get routing information.
 The data is saved in the file specified by foutput. Generally used to start
 operating with an EIDA default configuration.
@@ -200,143 +194,17 @@ operating with an EIDA default configuration.
     logs.info('Routing information succesfully read from Arclink!\n')
 
 
-def getArcInv(arcServ='eida.gfz-potsdam.de', arcPort=18001):
-    """Connects via telnet to an Arclink server to get inventory information.
-The data is saved in the file ``Arclink-inventory.xml``. Generally used to
-start operating with an EIDA default configuration.
-
-:param arcServ: Arclink server address
-:type arcServ: str
-:param arcPort: Arclink server port
-:type arcPort: int
-
-.. deprecated:: since version 1.0.2
-
-.. warning::
-
-    In the future this method should not be used and the configuration should
-    be independent from Arclink. Namely, the ``routing.xml`` file must exist in
-    advance.
-
-    """
-
-    logs = logging.getLogger('getArcInv')
-
-    logs.warning('This function should probably not be used! Be carefull!')
-
-    tn = telnetlib.Telnet(arcServ, arcPort)
-    tn.write('HELLO\n')
-    # FIXME The institution should be detected here. Shouldn't it?
-    logs.info(tn.read_until('GFZ', 5))
-    tn.write('user routing@eida\n')
-    logs.debug('\nuser routing@eida')
-    logs.debug(tn.read_until('OK', 5))
-    tn.write('request inventory\n')
-    logs.debug('\nrequest inventory')
-    logs.debug(tn.read_until('OK', 5))
-    tn.write('1920,1,1,0,0,0 2030,1,1,0,0,0 * * * *\nEND\n')
-    logs.debug('\n1920,1,1,0,0,0 2030,1,1,0,0,0 * * * *\nEND')
-
-    reqID = 0
-    while not reqID:
-        text = tn.read_until('\n', 5).splitlines()
-        for line in text:
-            try:
-                testReqID = int(line)
-            except:
-                continue
-            if testReqID:
-                reqID = testReqID
-
-    logs.debug('status %s\n' % reqID)
-    myStatus = 'UNSET'
-    logs.debug('\n' + myStatus)
-    while (myStatus in ('UNSET', 'PROCESSING')):
-        sleep(1)
-        tn.write('status %s\n' % reqID)
-        stText = tn.read_until('END', 15)
-
-        stStr = 'status='
-        oldStatus = myStatus
-        myStatus = stText[stText.find(stStr) + len(stStr):].split()[0]
-        myStatus = myStatus.replace('"', '').replace("'", "")
-        if myStatus == oldStatus:
-            logs.debug('.')
-        else:
-            logs.debug('\n' + myStatus)
-
-    if myStatus != 'OK':
-        logs.error('Error! Request status is not OK.\n')
-        return
-
-    tn.write('download %s\n' % reqID)
-    logs.debug('download %s\n' % reqID)
-    here = os.path.dirname(__file__)
-    try:
-        os.remove(os.path.join(here, 'Arclink-inventory.xml.download'))
-    except:
-        pass
-
-    # Write the downloaded file
-
-    with open(os.path.join(here, 'Arclink-inventory.xml.download'), 'w') \
-            as fout:
-        #try:
-        fd = tn.get_socket().makefile('rb+')
-        # Read the size of the inventory
-        length = fd.readline(100).strip()
-        # Max number of retries
-        maxRet = 8
-        while not isinstance(length, int) and maxRet:
-            try:
-                length = int(length)
-            except:
-                sleep(1)
-                tn.write('download %s\n' % reqID)
-                logs.debug('Retrying! download %s\n' % reqID)
-                length = fd.readline(100).strip()
-                maxRet -= 1
-
-        logs.info('\nExpected size: %s\n' % length)
-        bytesRead = 0
-        while bytesRead < length:
-            buf = fd.read(min(4096, length - bytesRead))
-            bytesRead += len(buf)
-            bar = '|' + '=' * int(bytesRead * 100 / (2 * length)) + \
-                ' ' * int((length - bytesRead) * 100 / (2 * length)) + '|'
-            logs.debug('\r%s' % bar)
-            fout.write(buf)
-
-        buf = fd.readline(100).strip()
-        if buf != "END" or bytesRead != length:
-            raise Exception('Wrong length!')
-        #finally:
-        #    tn.write('PURGE %d\n' % reqID)
-
-    try:
-        os.rename(os.path.join(here, './Arclink-inventory.xml'),
-                  os.path.join(here, './Arclink-inventory.xml.bck'))
-    except:
-        pass
-
-    try:
-        os.rename(os.path.join(here, './Arclink-inventory.xml.download'),
-                  os.path.join(here, './Arclink-inventory.xml'))
-    except:
-        pass
-
-    logs.info('\nInventory read from Arclink!\n')
-
-
 def main():
     # FIXME logLevel must be used via argparser
     # Check verbosity in the output
-    parser = argparse.ArgumentParser(description='Get EIDA routing configuration and "export" it to the FDSN-WS style.')
+    desc = 'Get EIDA routes and "export" them to the FDSN-WS style.'
+    parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-l', '--loglevel',
                         help='Verbosity in the output.',
-                        choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'])
+                        choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO',
+                                 'DEBUG'])
     parser.add_argument('-s', '--server',
-            help='Arclink server address (address.domain:18001).')
+                        help='Arclink server address (address.domain:18001).')
     parser.add_argument('-c', '--config',
                         help='Config file to use.',
                         default='../ownDC.cfg')
@@ -374,19 +242,21 @@ def main():
             arcServ = 'eida.gfz-potsdam.de'
             arcPort = 18002
 
-
     getArcRoutes(arcServ, arcPort, 'ownDC-routes-tmp.xml')
-    #getArcInv(arcServ, arcPort)
+    logs.debug('Translating Arclink routes to FDSN WS')
     arc2fdsnws('ownDC-routes-tmp.xml', 'ownDC-routes.xml', config=args.config)
     try:
+        logs.debug('Remove temporary files (1/2)')
         os.remove('ownDC-routes-tmp.xml')
     except:
         pass
 
     try:
+        logs.debug('Remove temporary files (2/2)')
         os.remove('ownDC-routes-tmp.xml.bin')
     except:
         pass
+
 
 if __name__ == '__main__':
     main()
