4f1dea435eb809326594262e7233659646b12aba
Javier Quinteros <javier@quinteros.eu>
2017-12-08 23:09:13 +0100

    Rename the '-' character from the file name.

--- setup.py
+++ setup.py
@@ -71,7 +71,8 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    #   py_modules=["my_module"],
+    # py_modules=["ownDC"],
+    provides=["ownDC"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
@@ -92,15 +93,18 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'sample': ['ownDC.cfg'],
-    },
+    # package_data={
+    #     'sample': ['ownDC.cfg'],
+    # },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[(path.join(path.expanduser('~'), '.ownDC'), ['ownDC.cfg'])],
+    data_files=[(path.join(path.expanduser('~'), '.ownDC'), ['ownDC.cfg']),
+                (path.join(path.expanduser('~'), '.ownDC', 'data'), ['data/ownDC-routes.xml']),
+                (path.join(path.expanduser('~'), '.ownDC', 'data'), ['data/masterTable.xml'])
+                ],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
@@ -108,5 +112,7 @@ setup(
     entry_points='''
         [console_scripts]
         ownDC=ownDC:main
+        ownDCcli=ownDCcli:main
+        ownDCupdate=ownDCupdate:main
     '''
 )
