c584be287588af37556c2e414be89b2fc0e13ce0
Javier Quinteros <javier@quinteros.eu>
2017-12-19 15:47:15 +0100

    Include again the configuration and XML files.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.1a1.dev12',
+    version='0.9.1a1.dev19',
 
     description='owndc: An FDSN Virtual Datacentre for SeisComP3',
     long_description=long_description,
@@ -94,17 +94,17 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'sample': ['application.wadl'],
+        'owndc': ['owndc/application.wadl'],
     },
     include_package_data=True,
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[(path.join(path.expanduser('~'), '.owndc'), ['owndc.cfg']),
-    #             (path.join(path.expanduser('~'), '.owndc', 'data'), ['data/owndc-routes.xml']),
-    #             (path.join(path.expanduser('~'), '.owndc', 'data'), ['data/masterTable.xml'])
-    #             ],
+    data_files=[(path.join('/', path.expanduser('~'), '.owndc'), ['owndc.cfg']),
+                (path.join('/', path.expanduser('~'), '.owndc', 'data'), ['data/owndc-routes.xml']),
+                (path.join('/', path.expanduser('~'), '.owndc', 'data'), ['data/masterTable.xml'])
+                ],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
